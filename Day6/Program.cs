fe2f1bc86111d076cbe5b341cbf905c0d3df7925
Zoltan Marton <martonzoltan4@gmail.com>
2023-12-06 15:09:10 +0200

    Solve day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,31 @@
+﻿List<(int Time, int Distance)> races = new()
+{
+    (71530, 940200)
+};
+
+SolveRaces();
+
+return;
+
+void SolveRaces()
+{
+    var result = 1;
+    foreach (var (time, distance) in races)
+    {
+        var beatTheRecord = 0;
+        for (var i = 1; i < time; i++)
+        {
+            var timeLeftToTravel = time - i;
+            var distanceTraveled = i * timeLeftToTravel;
+            if (distanceTraveled > distance)
+            {
+                beatTheRecord++;
+            }
+        }
+
+        if (beatTheRecord <= 0) continue;
+        result *= beatTheRecord;
+    }
+
+    Console.WriteLine(result);
+}
