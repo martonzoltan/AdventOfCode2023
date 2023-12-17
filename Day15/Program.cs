538c1feda61177ea65a97138d0963ed2920b2662
Zoltan Marton <martonzoltan4@gmail.com>
2023-12-17 19:14:13 +0200

    Solve day 15 and 16

---
+++ Day15/Program.cs
@@ -0,0 +1,100 @@
+﻿using Core;
+
+var input = InputHelper.GetInput().FirstOrDefault();
+if (input is null) throw new ArgumentException();
+
+//Part1();
+Part2();
+
+return;
+
+void Part1()
+{
+    var steps = input.Split(",");
+    var sum = 0;
+    foreach (var step in steps)
+    {
+        sum += GetHashValue(step);
+    }
+
+    Console.WriteLine(sum);
+}
+
+void Part2()
+{
+    var steps = input.Split(",");
+    var boxes = new Dictionary<int, List<Lens>>();
+    foreach (var step in steps)
+    {
+        string label;
+        if (step.Split('=').Length == 2)
+        {
+            label = step.Split('=')[0];
+            var focalLength = int.Parse(step.Split('=')[1]);
+            var lensToAdd = new Lens(label, focalLength);
+            var boxPosition = GetHashValue(label);
+            if (!boxes.ContainsKey(boxPosition))
+            {
+                boxes[boxPosition] = [];
+            }
+
+            var boxToUpdate = boxes[boxPosition].FirstOrDefault(x => x.Label == label);
+            if (boxToUpdate is null)
+            {
+                boxes[boxPosition].Add(lensToAdd);
+            }
+            else
+            {
+                boxToUpdate.FocalLength = lensToAdd.FocalLength;
+            }
+        }
+        else
+        {
+            label = step.TrimEnd('-');
+            var boxPosition = GetHashValue(label);
+            if (boxes.ContainsKey(boxPosition))
+            {
+                var boxToRemove = boxes[boxPosition].FirstOrDefault(x => x.Label == label);
+                if (boxToRemove is not null)
+                {
+                    boxes[boxPosition].Remove(boxToRemove);
+                }
+            }
+        }
+    }
+
+    var sum = 0;
+    foreach (var box in boxes)
+    {
+        var boxValue = 0;
+        var i = 1;
+        foreach (var lens in box.Value)
+        {
+            boxValue += (1 + box.Key) * i * lens.FocalLength;
+            i++;
+        }
+
+        sum += boxValue;
+    }
+
+    Console.WriteLine(sum);
+}
+
+int GetHashValue(string step)
+{
+    var hash = 0;
+    foreach (var character in step)
+    {
+        hash += character;
+        hash *= 17;
+        hash %= 256;
+    }
+
+    return hash;
+}
+
+internal sealed class Lens(string Label, int FocalLength)
+{
+    public string Label { get; } = Label;
+    public int FocalLength { get; set; } = FocalLength;
+}
---
+++ Day16/Program.cs
@@ -0,0 +1,345 @@
+﻿using Core;
+
+var input = InputHelper.GetMatrixInput();
+var energizedFields = new bool[input.GetLength(0), input.GetLength(1)];
+HashSet<(int x, int y, Direction dir)> visited = [];
+
+Part1();
+Part2();
+
+return;
+
+void Part1()
+{
+    energizedFields[0, 0] = true;
+    MoveRay(new Ray(0, 0, GetStartingPositionDirection(0, 0, Direction.Right)));
+
+    var sum = 0;
+    for (var i = 0; i < energizedFields.GetLength(0); i++)
+    {
+        for (var j = 0; j < energizedFields.GetLength(1); j++)
+        {
+            if (energizedFields[i, j])
+                sum++;
+        }
+    }
+
+    Console.Write(sum);
+}
+
+void Part2()
+{
+    var sum = 0;
+
+    for (var i = 0; i < energizedFields.GetLength(0); i++)
+    {
+        energizedFields = new bool[input.GetLength(0), input.GetLength(1)];
+        var localSum = 0;
+        visited = [];
+
+        energizedFields[0, i] = true;
+        var startingDir = GetStartingPositionDirection(0, i, Direction.Down);
+        MoveRay(new Ray(0, i, startingDir));
+
+        for (var ii = 0; ii < energizedFields.GetLength(0); ii++)
+        {
+            for (var jj = 0; jj < energizedFields.GetLength(1); jj++)
+            {
+                if (energizedFields[ii, jj])
+                    localSum++;
+            }
+        }
+
+        if (localSum > sum)
+        {
+            sum = localSum;
+        }
+    }
+
+    for (var i = 0; i < energizedFields.GetLength(0); i++)
+    {
+        energizedFields = new bool[input.GetLength(0), input.GetLength(1)];
+        var localSum = 0;
+        visited = [];
+
+        energizedFields[i, energizedFields.GetLength(0) - 1] = true;
+        var startingDir = GetStartingPositionDirection(i, energizedFields.GetLength(0) - 1, Direction.Left);
+        MoveRay(new Ray(i, energizedFields.GetLength(0) - 1, startingDir));
+
+        for (var ii = 0; ii < energizedFields.GetLength(0); ii++)
+        {
+            for (var jj = 0; jj < energizedFields.GetLength(1); jj++)
+            {
+                if (energizedFields[ii, jj])
+                    localSum++;
+            }
+        }
+
+        if (localSum > sum)
+        {
+            sum = localSum;
+        }
+    }
+
+    for (var i = input.GetLength(0) - 1; i >= 0; i--)
+    {
+        energizedFields = new bool[input.GetLength(0), input.GetLength(1)];
+        var localSum = 0;
+        visited = [];
+
+        energizedFields[input.GetLength(0) - 1, i] = true;
+        var startingDir = GetStartingPositionDirection(input.GetLength(0) - 1, i, Direction.Up);
+        MoveRay(new Ray(input.GetLength(0) - 1, i, startingDir));
+
+        for (var ii = 0; ii < energizedFields.GetLength(0); ii++)
+        {
+            for (var jj = 0; jj < energizedFields.GetLength(1); jj++)
+            {
+                if (energizedFields[ii, jj])
+                    localSum++;
+            }
+        }
+
+        if (localSum > sum)
+        {
+            sum = localSum;
+        }
+    }
+
+    for (int i = input.GetLength(0) - 1; i >= 0; i--)
+    {
+        energizedFields = new bool[input.GetLength(0), input.GetLength(1)];
+        var localSum = 0;
+        visited = [];
+
+        energizedFields[i, 0] = true;
+        var startingDir = GetStartingPositionDirection(i, 0, Direction.Right);
+        MoveRay(new Ray(i, 0, startingDir));
+
+        for (var ii = 0; ii < energizedFields.GetLength(0); ii++)
+        {
+            for (var jj = 0; jj < energizedFields.GetLength(1); jj++)
+            {
+                if (energizedFields[ii, jj])
+                    localSum++;
+            }
+        }
+
+        if (localSum > sum)
+        {
+            sum = localSum;
+        }
+    }
+
+    Console.Write(sum);
+}
+
+void MoveRay(Ray ray)
+{
+    if (!visited.Add((ray.X, ray.Y, ray.Direction)))
+    {
+        return;
+    }
+
+    switch (ray.Direction)
+    {
+        case Direction.Left:
+            ray.Y--;
+            if (MatrixHelper.IsInsideMatrix(input, ray.X, ray.Y))
+            {
+                energizedFields[ray.X, ray.Y] = true;
+                switch (input[ray.X, ray.Y])
+                {
+                    case '.':
+                    case '-':
+                        break;
+                    case '/':
+                        ray.Direction = Direction.Down;
+                        break;
+                    case '\\':
+                        ray.Direction = Direction.Up;
+                        break;
+                    case '|':
+                        MoveRay(new Ray(ray.X, ray.Y, Direction.Down));
+                        ray.Direction = Direction.Up;
+                        break;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            break;
+        case Direction.Down:
+            ray.X++;
+            if (MatrixHelper.IsInsideMatrix(input, ray.X, ray.Y))
+            {
+                energizedFields[ray.X, ray.Y] = true;
+                switch (input[ray.X, ray.Y])
+                {
+                    case '.':
+                    case '|':
+                        break;
+                    case '/':
+                        ray.Direction = Direction.Left;
+                        break;
+                    case '\\':
+                        ray.Direction = Direction.Right;
+                        break;
+                    case '-':
+                        MoveRay(new Ray(ray.X, ray.Y, Direction.Right));
+                        ray.Direction = Direction.Left;
+                        break;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            break;
+        case Direction.Right:
+            ray.Y++;
+            if (MatrixHelper.IsInsideMatrix(input, ray.X, ray.Y))
+            {
+                energizedFields[ray.X, ray.Y] = true;
+                switch (input[ray.X, ray.Y])
+                {
+                    case '.':
+                    case '-':
+                        break;
+                    case '/':
+                        ray.Direction = Direction.Up;
+                        break;
+                    case '\\':
+                        ray.Direction = Direction.Down;
+                        break;
+                    case '|':
+                        MoveRay(new Ray(ray.X, ray.Y, Direction.Down));
+                        ray.Direction = Direction.Up;
+                        break;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            break;
+        case Direction.Up:
+            ray.X--;
+            if (MatrixHelper.IsInsideMatrix(input, ray.X, ray.Y))
+            {
+                energizedFields[ray.X, ray.Y] = true;
+                switch (input[ray.X, ray.Y])
+                {
+                    case '.':
+                    case '|':
+                        break;
+                    case '/':
+                        ray.Direction = Direction.Right;
+                        break;
+                    case '\\':
+                        ray.Direction = Direction.Left;
+                        break;
+                    case '-':
+                        MoveRay(new Ray(ray.X, ray.Y, Direction.Right));
+                        ray.Direction = Direction.Left;
+                        break;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            break;
+    }
+
+    MoveRay(new Ray(ray.X, ray.Y, ray.Direction));
+}
+
+Direction GetStartingPositionDirection(int i, int j, Direction initDirection)
+{
+    switch (initDirection)
+    {
+        case Direction.Left:
+            switch (input[i, j])
+            {
+                case '.':
+                case '-':
+                    return Direction.Left;
+                case '/':
+                    return Direction.Down;
+                case '\\':
+                    return Direction.Up;
+                case '|':
+                    return Direction.Down;
+            }
+
+            break;
+        case Direction.Down:
+            switch (input[i, j])
+            {
+                case '.':
+                case '|':
+                    return Direction.Down;
+                case '/':
+                    return Direction.Left;
+                case '\\':
+                    return Direction.Right;
+                case '-':
+                    return Direction.Left;
+            }
+
+            break;
+        case Direction.Right:
+            switch (input[i, j])
+            {
+                case '.':
+                case '-':
+                    return Direction.Right;
+                case '/':
+                    return Direction.Up;
+                case '\\':
+                    return Direction.Down;
+                case '|':
+                    return Direction.Up;
+            }
+
+            break;
+        case Direction.Up:
+            switch (input[i, j])
+            {
+                case '.':
+                case '|':
+                    return Direction.Up;
+                case '/':
+                    return Direction.Right;
+                case '\\':
+                    return Direction.Left;
+                case '-':
+                    return Direction.Right;
+            }
+
+            break;
+    }
+
+    return Direction.Right;
+}
+
+internal sealed class Ray(int x, int y, Direction direction)
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
+    public Direction Direction { get; set; } = direction;
+}
+
+internal enum Direction
+{
+    Left,
+    Down,
+    Right,
+    Up,
+}
